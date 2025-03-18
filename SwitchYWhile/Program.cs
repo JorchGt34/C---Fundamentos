f2d6480dbdeb8e68c458a9c01ee2bfd1fd113a35
Jorch34 <jorchgt34@gmail.com>
2025-03-18 17:53:23 -0600

    Uso del ciclo while y do-while en el c odigo

---
+++ SwitchYWhile/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+int totalJugador, totalDealer, num;
+string mensaje, controlSwitch = "menu",endGame = "si", controlOtraCarta;
+System.Random random = new System.Random();
+
+while(endGame == "si"){
+    totalJugador = 0; totalDealer = 0;
+    switch (controlSwitch) {
+    case "menu":
+        Console.WriteLine("Bienvenido al juego de Blackjack \n Escriba 21 para comenzar");
+        controlSwitch = Console.ReadLine();
+        break;
+    case "21":
+    //El jugador toma cartas
+        do {
+            num = random.Next(1,12);
+            totalJugador += num;
+            Console.WriteLine($"Nueva carta del jugador: {num}");
+            Console.WriteLine($"Total del jugador: {totalJugador}");
+            Console.WriteLine("¿Desea otra carta?");
+            controlOtraCarta = Console.ReadLine();
+        } while (controlOtraCarta == "Otra carta" || controlOtraCarta == "Si" || controlOtraCarta == "si" || controlOtraCarta == "yes");
+    //El dealer toma cartas
+        do {
+            num = random.Next(1,12);
+            totalDealer += num;
+            Console.WriteLine($"Nueva carta del dealer: {num}");
+            Console.WriteLine($"Total del dealer: {totalDealer}");
+        } while (totalDealer < 17);
+
+    //Determinar ganador
+        if (totalJugador > totalDealer && totalJugador <= 21 || totalDealer > 21 && totalJugador <= 21){
+        mensaje = "¡Felicidades! Has ganado";
+        } else {
+            mensaje = "¡Lo siento! Has perdido";
+        }
+        Console.WriteLine(mensaje);
+        Console.WriteLine("¿Desea jugar de nuevo?\n-no para terminar\n-si para continuar");
+        endGame = Console.ReadLine();
+        break;
+    default:
+        mensaje = "Opción no válida";
+        break;
+    }
+}
