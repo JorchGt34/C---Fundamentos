955a94fe87dd600be55f339a92920992ab770b3b
Jorch34 <jorchgt34@gmail.com>
2025-03-17 23:38:33 -0600

    Nueva aplicacion de consola para probar los operadores logicos

--- AreaRectangulo/Program.cs
+++ AreaRectangulo/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Programa que calcula el área de un rectangulo
-var ladoA = 0.0;
-var ladoB = 0.0;
-var resultado = 0.0;
+var ladoA = 0d;
+var ladoB = 0d;
+var resultado = 0d;
 
 Console.WriteLine("Calcula el área de un rectangulo");
 Console.WriteLine("Ingresa el valor de lado A");
@@ -9,6 +9,26 @@ ladoA = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Ingresa el valor de lado B");
 ladoB = Convert.ToDouble(Console.ReadLine());
+
+//Operadores aritmeticos en C#
+/*
+ladoA + ladoB = Suma
+ladoA - ladoB = Resta
+ladoA * ladoB = Multiplicación
+ladoA / ladoB = División
+ladoA % ladoB = Módulo
+*/
+
+//Otros operadores
+/*
+ladoA++ = Incremento
+ladoA-- = Decremento
+ladoA += ladoB = ladoA = ladoA + ladoB
+ladoA -= ladoB = ladoA = ladoA - ladoB
+ladoA *= ladoB = ladoA = ladoA * ladoB
+ladoA /= ladoB = ladoA = ladoA / ladoB
+ladoA %= ladoB = ladoA = ladoA % ladoB
+*/
 
 resultado = ladoA * ladoB;
 Console.WriteLine("El lado A es de " + ladoA + " mientras que el lado B es de " + ladoB + " por lo que el área del rectangulo es de: " + resultado);
---
+++ OperadoresLogicos/Program.cs
@@ -0,0 +1,27 @@
+﻿//Operadores logicos
+/*
+&& - AND
+|| - OR
+! - NOT 
+^ - XOR (Ou exclusivo)
+*/
+
+var (valor1, valor2, valor3) = (true, true, false);
+
+//Operador AND
+bool resultOr = valor1 && valor2 && valor3;
+Console.WriteLine(resultOr);
+
+//Operador OR
+bool resultAnd = valor1 || valor2 || valor3;
+Console.WriteLine(resultAnd);
+
+//Operador AndOr
+bool resultAndOr = (valor1 && valor2) || valor3;
+Console.WriteLine(resultAndOr);
+
+bool resultNot = !valor1;
+Console.WriteLine(resultNot);
+
+bool resultXOR = valor1 ^ valor2 ^ valor3; //Primero checa los dos primeros valores y luego el resultado con el tercer valor
+Console.WriteLine(resultXOR);
