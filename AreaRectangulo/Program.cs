55aac78c51054a89cc79099251807cbcb4d35050
Jorch34 <jorchgt34@gmail.com>
2025-03-17 20:41:49 -0600

    Se agrego un programa que calcula el area de un cirulo

---
+++ AreaRectangulo/Program.cs
@@ -0,0 +1,14 @@
+﻿//Programa que calcula el área de un rectangulo
+var ladoA = 0.0;
+var ladoB = 0.0;
+var resultado = 0.0;
+
+Console.WriteLine("Calcula el área de un rectangulo");
+Console.WriteLine("Ingresa el valor de lado A");
+ladoA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingresa el valor de lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+Console.WriteLine("El lado A es de " + ladoA + " mientras que el lado B es de " + ladoB + " por lo que el área del rectangulo es de: " + resultado);
---
+++ Circle/Program.cs
@@ -0,0 +1,12 @@
+﻿var radio = 0d;
+var resultado = 0d;
+
+Console.WriteLine("Bienvenido, calculemos círculos");
+// Pi * Radio^2
+
+Console.WriteLine("Ingrese el redio del circulo:");
+radio = Convert.ToDouble(Console.ReadLine());
+
+resultado = Math.PI * Math.Pow(radio, 2);
+
+Console.WriteLine("El área del círculo es: " + resultado);
