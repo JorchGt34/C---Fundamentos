0419aa42345de5b6db8b62e223206d3eb31e190a
Jorch34 <jorchgt34@gmail.com>
2025-03-17 20:09:58 -0600

    Tipos de datos primitivos - ejemplo para calcular el area de un rectangulo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Programa que calcula el área de un rectangulo
+double ladoA;
+double ladoB;
+double resultado;
+
+Console.WriteLine("Calcula el área de un rectangulo");
+Console.WriteLine("Ingresa el valor de lado A");
+ladoA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingresa el valor de lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+Console.WriteLine("El lado A es de " + ladoA + " mientras que el lado B es de " + ladoB + " por lo que el área del rectangulo es de: " + resultado);
