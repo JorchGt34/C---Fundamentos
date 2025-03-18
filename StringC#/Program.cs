1e880addb8b1b99fc82a536a4cec37f88e1f9251
Jorch34 <jorchgt34@gmail.com>
2025-03-18 10:14:00 -0600

    Se agrego el uso de Strings en C#

---
+++ StringC#/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Jorge Montoya ID!");
+int altura = 182;
+int edad = 23;
+string nombre = "Jorge Montoya";
+string informacion = "Nació en Hospital Hidalgo, es estudiante de desarrollo de software y computacion";
+var hobby = "Jugar videojuegos";
+
+string tarjetaDeIdentificacion = $"La información de {nombre} es la siguiente: \n Edad de: {edad} años \n Su altura es de: {altura} cm\n Información relevante: {informacion} \n Su hobby es {hobby}";
+
+Console.WriteLine(tarjetaDeIdentificacion);
