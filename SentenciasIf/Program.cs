7f4f3c251982d5057d20f7cffc554a22ad7c325f
Jorch34 <jorchgt34@gmail.com>
2025-03-18 11:03:50 -0600

    Se agrego un archivo expicando sentencias if en C#

---
+++ SentenciasIf/Program.cs
@@ -0,0 +1,15 @@
+﻿//Blackjack
+int totalJugador = 22;
+int totalDealer = 16;
+string mensaje = "";
+
+//Blackjack
+//El jugador debe tener un total de 21 o menos para poder seguir jugando
+
+if ((totalJugador <= 21 && totalJugador > totalDealer) || (totalDealer > 21 && totalJugador <= 21)) {
+    mensaje = "El jugador gana";
+} else {
+    mensaje = "El dealer gana";
+}
+
+Console.WriteLine(mensaje);
