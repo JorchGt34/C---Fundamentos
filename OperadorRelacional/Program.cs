0b2a72341d4e0216f0656e27b567a3852787742b
Jorch34 <jorchgt34@gmail.com>
2025-03-17 23:55:57 -0600

    Se agrego una aplicacion de consola explicando las comparaciones

---
+++ OperadorRelacional/Program.cs
@@ -0,0 +1,30 @@
+﻿var (valor1, valor2, valor3) = (10, 10, 20);
+Console.WriteLine("Los valores son: " + valor1 + ", " + valor2 + ", " + valor3);
+
+//Operadores relacionales
+/*
+== Igual a
+!= Diferente de 
+> Mayor que
+< Menor que
+>= Mayor o igual que
+<= Menor o igual que
+*/
+
+bool resultado1 = valor1 == valor2;
+Console.WriteLine("La comparacion de valor1 == valor2 es: " + resultado1);
+
+bool resultado2 = valor1 != valor2;
+Console.WriteLine("La comparacion de valor1 != valor2 es: " + resultado2);
+
+bool resultado3 = valor1 > valor2;
+Console.WriteLine("La comparacion de valor1 > valor2 es: " + resultado3);
+
+bool resultado4 = valor1 < valor2;
+Console.WriteLine("La comparacion de valor1 < valor2 es: " + resultado4);
+
+bool resultado5 = valor1 >= valor2;
+Console.WriteLine("La comparacion de valor1 >= valor2 es: " + resultado5);
+
+bool resultado6 = valor1 <= valor2;
+Console.WriteLine("La comparacion de valor1 <= valor2 es: " + resultado6);
